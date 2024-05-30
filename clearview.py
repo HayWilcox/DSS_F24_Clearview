0cd24294057ba3f8259c9bc975b685fb8f8bd917
AsaRy13 <167818887+AsaRy13@users.noreply.github.com>
2024-05-29 19:42:01 -0600

    Made it so clearview.py can modify the color table in the database.

--- clearview.py
+++ clearview.py
@@ -1,4 +1,5 @@
 import os
+import mysql.connector
 
 try:
     import streamlit as st
@@ -8,3 +9,20 @@ except ImportError:
     import streamlit as st
     
 st.write('Hello Team!')
+
+clearview = mysql.connector.connect(
+    host = "localhost",
+    user="clearview",
+    password="CView24",
+    database = 'clearview'
+)
+
+cvcursor = clearview.cursor()
+
+color = st.text_input('Please enter a color: ')
+color_button = st.button('Submit', key=1)
+
+if color_button:
+    cvcursor.execute('INSERT INTO color (color_name) VALUES (%s)', (color,))
+    clearview.commit()
+    st.write('successful')
