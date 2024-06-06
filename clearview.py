a26a947042ce74adc327df6f84caf9ee076cd1bf
Kemish Gomez <113635157+Kemishigp@users.noreply.github.com>
2024-06-05 18:15:36 -0600

    commit

--- clearview.py
+++ clearview.py
@@ -1,4 +1,6 @@
+
 import os
+import mysql.connector
 
 try:
     import streamlit as st
@@ -6,5 +8,19 @@ try:
 except ImportError:
     os.system('pip install streamlit')
     import streamlit as st
-    
-st.write('Hello Team!')
+    st.write('Hello Team!')
+
+clearview = mysql.connector.connect(
+    host = "localhost",
+    user="clearview",
+    password = "CView24",
+)
+
+cvcursor = clearview.cursor()
+color = st.text_input('Please enter a color: ')
+color_button = st.button('Submit', key =1)
+
+if color_button:
+    cvcursor.execute('INSERT INTO color (color_name) VALUES (%s)', (color,))
+    clearview.commit()
+    st.write(cvcursor.execute('SELECT * FROM color;'))
