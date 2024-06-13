2a6f1f2cf207009ea3321d5043c22f9fce0c351f
GituserKok <167933365+GituserKok@users.noreply.github.com>
2024-06-12 18:15:21 -0600

    roger_stuff

---
+++ mesh.py
@@ -0,0 +1,27 @@
+import pandas as pd
+class mesh:
+
+    def __init__(self, cvcursor, cvconn):
+        self.cvcursor = cvcursor
+        self.cvconn = cvconn
+
+    def insert_mesh(self, mesh):
+        self.cvcursor.execute('INSERT INTO mesh (mesh_type) VALUES (%s)', (mesh,))
+        self.cvconn.commit()
+
+    def select_mesh(self):
+        self.cvcursor.execute('SELECT * FROM mesh')
+        return self.cvcursor.fetchall()
+    
+    def display_mesh(self):
+        df = pd.DataFrame(self.select_mesh())
+        df.columns = ['Mesh Id', 'Mesh']
+        return df
+    
+    def update_mesh(self, mesh_id, mesh):
+        self.cvcursor.execute('UPDATE mesh SET mesh_type = %s WHERE mesh_id = %s', (mesh, mesh_id))
+        self.cvconn.commit()
+
+    def delete_mesh(self, mesh_id):
+        self.cvcursor.execute('DELETE FROM mesh WHERE mesh_id = %s', (mesh_id,))
+        self.cvconn.commit()
