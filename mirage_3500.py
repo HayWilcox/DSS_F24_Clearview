e09401f2bd6fd831b49176c152a38d03451863a9
Nicholas Boss <86071846+NicholasBoss@users.noreply.github.com>
2024-06-22 09:54:53 -0600

    Fixed display_mirage_3500.

--- mirage_3500.py
+++ mirage_3500.py
@@ -20,7 +20,7 @@ class mirage_3500:
         return self.cvcursor.fetchall()
     
     def display_mirage_3500(self):
-        df = pd.DataFrame(self.select_color())
+        df = pd.DataFrame(self.select_mirage_3500())
         df.columns = ['Mirage 3500 Id', 'Mirage 3500 Handle']
         return df
     
